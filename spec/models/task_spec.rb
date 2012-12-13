f350ba9fdc4c13746d9961c324759e58e3286402
Jevon Yeoh <jevonyeoh@gmail.com>
2012-12-13 15:26:15 -0500

    final

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Task do
-    it "requires a name and description" do
+    it "requires a name" do
         subject.should_not be_valid
         subject.name = "my first task"
         subject.should be_valid
